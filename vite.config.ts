11a34f2321fe23bf2b1fc8d551345ad071bc0f2d
Hsinah <hanishthiyagarajan5@gmail.com>
2026-02-22 00:51:59 +0530

    fix: router and github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "./hanish-portfolio",
+  base: "/Portfolio-Website-/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
